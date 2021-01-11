eda158a1af7d1360f3f3ab1ce15ef4e5f439047e
Sergio Perez <sergiombp@Sergios-Mac.local>
2021-01-11 15:59:56 -0500

    material-ui slider

--- client/src/components/modules/HostGame.jsx
+++ client/src/components/modules/HostGame.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
+import Slider from '@material-ui/core/Slider';
+
+// marks for material-ui slider
+// currently disabled, can enable in props of slider
+// marks = {marks}
+const marks = [
+    {
+        value: 0,
+        label: '0',
+    },
+    {
+        value: 10,
+        label: '10',
+    },
+];
 
 export class HostGame extends Component {
     constructor(props) {
         super(props);
         this.state = {
             name: null,
+            capacity: 3,
+            public: false,
         }
     }
 
     inputName = (event) => {
         this.setState({name: event.target.value});
+    }
+
+    changeCapacity = (event, value) => {
+        this.setState({capacity: value})
+    }
+
+    togglePrivacy = () => {
+        this.setState({public: !this.state.public})
     }
 
     handleSubmit = (event) => {
@@ -23,6 +48,20 @@ export class HostGame extends Component {
                 <form onSubmit={this.handleSubmit}>          
                     <input type="text" value={this.state.name} placeholder="Game Name" onChange={this.inputName} />
                 </form>
+
+                {/* Add Privacy Toggle */}
+
+                <h5 style ={{color: "white"}}>Capacity</h5>    
+                <Slider 
+                    defaultValue={3}
+                    aria-labelledby="discrete-slider"
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={3}
+                    max={10}
+                    onChange={this.changeCapacity}
+                />
             </div>
         )
     }
