460a7b8d479df0711e08b98bca1e6a4b319fdcbb
Naseem Hamed <naseemhamed@Naseems-MacBook-Pro-2.local>
2021-01-15 00:04:38 -0500

    added state of sliders to lobby -H

--- client/src/components/pages/Lobby.js
+++ client/src/components/pages/Lobby.js
@@ -9,22 +9,9 @@ class Lobby extends Component {
   // makes props available in this component
   constructor(props) {
     super(props);
-    this.state = {};
-  }
 
-  componentDidMount() {
-    //get("/api/lobby", { gameId: this.props.gameId })
-    //  .then((res) => {
-    //    this.setState({
-    //      lobby: res.lobby,
-    //    });
-    //  })
-    //  .then(() => console.log(this.state.lobby));
-  }
-
-  render() {
     // The array follows this schema: [min, default, max, step-size]
-    const settings = {
+    this.settings = {
       "General Settings": { "Time Limit": [2, 5, 10, 1], "Map Size": [1, 1, 3, 1] },
       "Marco Settings": {
         "Vision Radius": [0, 50, 100, 5],
@@ -37,6 +24,30 @@ class Lobby extends Component {
       },
     };
 
+    this.state = { sliders: this.resetSettings() };
+  }
+
+  resetSettings = () => {
+    let tempSliderState = {};
+    Object.keys(this.settings).map((type) => {
+      Object.keys(this.settings[type]).map((setting, index) => {
+        tempSliderState[type + setting + index] = this.settings[type][setting][1];
+      });
+    });
+    return tempSliderState;
+  };
+
+  componentDidMount() {
+    //get("/api/lobby", { gameId: this.props.gameId })
+    //  .then((res) => {
+    //    this.setState({
+    //      lobby: res.lobby,
+    //    });
+    //  })
+    //  .then(() => console.log(this.state.lobby));
+  }
+
+  render() {
     return (
       <div className="lobby-base">
         <div className="lobby-container">
@@ -46,32 +57,42 @@ class Lobby extends Component {
               <div className="lobby-content-header-name">Lobby One</div>
               <div className="lobby-content-header-buttoncontainer">
                 <div className="lobby-content-header-playercount">10/10</div>
-                <button className="lobby-content-header-reset" type="button">
+                <button
+                  className="lobby-content-header-reset"
+                  type="button"
+                  onClick={() => {
+                    this.setState({ sliders: this.resetSettings() });
+                  }}
+                >
                   Reset Settings
                 </button>
               </div>
             </div>
             <div className="lobby-content-settings">
-              {Object.keys(settings).map((type, indexFirst) => (
+              {Object.keys(this.settings).map((type, indexFirst) => (
                 <>
                   <div className="lobby-content-settings-type" key={"Type " + indexFirst}>
                     {type}
                   </div>
-                  {Object.keys(settings[type]).map((setting, index) => (
+                  {Object.keys(this.settings[type]).map((setting, index) => (
                     <div
                       className="lobby-content-setting-slidercontainer"
                       key={"SliderCont " + index}
                     >
                       <Slider
                         className="lobby-content-setting-slider"
-                        defaultValue={settings[type][setting][1]}
+                        value={this.state.sliders[type + setting + index]}
                         aria-labelledby="discrete-slider"
                         valueLabelDisplay="on"
-                        step={settings[type][setting][3]}
-                        min={settings[type][setting][0]}
-                        max={settings[type][setting][2]}
-                        onChange={this.changeCapacity}
-                        key={"Slider " + index}
+                        step={this.settings[type][setting][3]}
+                        min={this.settings[type][setting][0]}
+                        max={this.settings[type][setting][2]}
+                        onChange={(event, value) => {
+                          let tempSliders = { ...this.state.sliders };
+                          tempSliders[type + setting + index] = value;
+                          this.setState({ sliders: tempSliders });
+                        }}
+                        key={type + setting + index}
                       />
                       <div key={"SliderName" + index}>{setting}</div>
                     </div>
