4ab656f9e81170e6535976418832e8fbb6df60ff
Naseem Hamed <naseemhamed@Naseems-MacBook-Pro-2.local>
2021-01-18 13:20:05 -0500

    f

--- client/src/components/modules/Message.js
+++ client/src/components/modules/Message.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 
-
 class Message extends Component {
-    constructor(props) {
-      super(props);
-    }
-  
-    render() {
-      return (
-        <div >
-          <span>{this.props.message.sender.name + ":"}</span>
-          <span>{this.props.message.content}</span>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
   }
-  
-  export default Message;
-  
+
+  render() {
+    return (
+      <div>
+        <span>{this.props.message.sender.name + ":"}</span>
+        <span>{this.props.message.content}</span>
+      </div>
+    );
+  }
+}
+
+export default Message;
