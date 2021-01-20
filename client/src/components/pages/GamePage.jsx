1e3928072564b07069d57ffbff18f36d79ca8322
Naseem Hamed <naseemhamed@Naseems-MacBook-Pro-2.local>
2021-01-19 19:01:32 -0500

    updated game-starter -H

--- client/src/components/pages/GamePage.jsx
+++ client/src/components/pages/GamePage.jsx
@@ -35,9 +35,8 @@ export class GamePage extends Component {
       }
       this.gameLoop();
     }, 1000 / 60);
-
-
      */
+
     requestAnimationFrame(() => {
       this.updatePosition();
       console.log("yes");
