c5760147e2c31b8b15f940a5122051b05dbcea4f
sabiyyah <65693581+sabiyyah@users.noreply.github.com>
2021-01-23 22:52:33 -0500

    imgs -A

---
+++ client/src/components/pages/assets/Inside_B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Inside_B" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../Downloads/CP_Interior1/Inside_B.png" width="768" height="768"/>
+</tileset>
